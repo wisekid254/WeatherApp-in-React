027f484f144f46969e03372ed787e7e91b882309
Tito <titusmunene100@gmail.com>
2025-06-27 14:06:02 +0300

    Data fetch and display on the UI successfully search icon working well

--- Weather/src/components/Weather.jsx
+++ Weather/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
@@ -10,6 +10,21 @@ import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 
 const Weather = () => {
+    const search = async (city)=>{
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+            
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            
+        } catch (error) {
+            
+        }
+    }
+    useEffect(()=>{
+        search("Nairobi");
+    }, [])
   return (
     <div className="weather">
       <div className="search-bar">
